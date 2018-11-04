4d871c803309f90b02248de78a7840076492d45e
evitacindy98 <44153558+evitacindy98@users.noreply.github.com>
2018-11-05 06:03:13 +0700

    tugas_modul3

---
+++ Modul-3-Kelompok-42/app/src/main/java/com/example/evitacindys/aplikasimodul2kelompok29/ui/detailCar/detailView.java
@@ -0,0 +1,12 @@
+package com.example.evitacindys.aplikasimodul2kelompok29.ui.detailCar;
+
+import com.example.evitacindys.aplikasimodul2kelompok29.data.model.DataCar;
+
+import java.util.List;
+
+public interface detailView {
+        void showErrorCarById(String message);
+
+        void showSuccessCarById(List<DataCar> dataC);
+
+}
